d3f12c9bc98ef83c37478a1241596878bf792675
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-02-23 15:55:31 -0800

    challenge one

---
+++ js/js-challenge.js
@@ -0,0 +1,20 @@
+//Excercise ONE
+
+const fullName = "Jeffrey Alexander Rios Jr";
+const welcomeMessage = ", Says Hello EveryBody!"
+const fullMessage = fullName + welcomeMessage;
+alert(fullMessage);
+
+
+//Excercise TWO
+
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+const num4 = 4;
+const num5 = 5;
+const num6 = 6;
+const answer1 = num1 + num2 + num3 + num4 + num5 + num6;
+console.log(answer1);
+const answer2 = (num1 + num2 + num3) * num4;
+console.log(answer2);
