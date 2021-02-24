2acc8c2ccc8a63919e60a5a63e47b20b9eccdc92
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-02-24 13:10:38 -0800

    finished 2nd ecercise

---
+++ js/js-challenge-two.js
@@ -0,0 +1,14 @@
+
+        //Excercise One :
+
+for (let i = 1; i <= 101; i++) {
+    if (i % 2 !== 0) console.log(i);
+}
+
+            //Excercise Two :
+
+            const welcome = (firstName, lastName) => {
+              alert(firstName + '' + lastName + ', welcome!');
+            }
+
+            welcome('Jeffrey',  'Rios');
